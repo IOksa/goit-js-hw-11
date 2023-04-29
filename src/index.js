57ff545de6c7a5231c9981d049969425002a852c
IOksa <117656767+IOksa@users.noreply.github.com>
2023-04-29 19:24:15 +0300

    update

--- src/index.js
+++ src/index.js
@@ -8,40 +8,61 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "35857316-b0404474f9c8f0cfe824c51d8";
+let countPage=1;
+let searchQuery="";
+let queryLimit=40;
 
 const refs={
     form: document.querySelector('#search-form'),
     input: document.querySelector('.search-input'),
     div: document.querySelector(".gallery"),
+    LoadMoreBtn: document.querySelector('.load-more'),
+    
 }
 
 let gallery = new SimpleLightbox('.gallery a');
 
-console.log(refs.form);
-console.log(refs.input);
+refs.LoadMoreBtn.hidden=true;
+
 refs.form.addEventListener('submit', onFormSubmit);
+refs.LoadMoreBtn.addEventListener('click', onClickLoadMore)
+
+// плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+///////////////////////////////////////////////////////////////////////////////////////////
 
 function onFormSubmit(event){
     event.preventDefault();
-    const form = event.currentTarget;
-    const searchQuery = form.elements.searchQuery.value;
-    console.log(searchQuery);
+    refs.LoadMoreBtn.hidden=true;
 
-    fetchQuery(searchQuery)
+    countPage=1;
+    refs.div.innerHTML="";
+
+    const form = event.currentTarget;
+    searchQuery = form.elements.searchQuery.value;
+
+    fetchQuery()
     .then(checkQueryResponse)
-    .catch(err=>console.error("ERROR!", err));;
+    .catch(err=>Notify.failure('Ooops! Something went wrong!'));
     
 }
 
-async function fetchQuery(searchQuery){
-    console.log("I am in fetchQuery");
-
+async function fetchQuery(){
     const params = new URLSearchParams({
         key: API_KEY,
         q: searchQuery,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        per_page: queryLimit,
+        page: countPage,
 
     });
 
@@ -52,7 +73,6 @@ async function fetchQuery(searchQuery){
 }
 
 function checkQueryResponse(queryResponse){
-    console.log("I am in checkQueryResponse");
     console.log("queryResponse", queryResponse);
     console.log("queryResponse.hits=",queryResponse.data.hits);
 
@@ -61,10 +81,21 @@ function checkQueryResponse(queryResponse){
     }
     else{
         const markup=createImageGalleryMarkup(queryResponse.data.hits);
-        console.log(markup);
         refs.div.insertAdjacentHTML('beforeend', markup);
         gallery.refresh();
-    
+        console.log("queryResponse.data.totalHits", queryResponse.data.totalHits, "  ", queryResponse.data.totalHits/queryLimit)
+        console.log("countPage=", countPage);
+        if(countPage>1){
+            const countImage=countPage*queryLimit;
+            Notify.info(`Hooray! We found ${countImage} images.`);
+        }
+
+        if(countPage<queryResponse.data.totalHits/queryLimit){
+            refs.LoadMoreBtn.hidden = false;
+        }else{
+            refs.LoadMoreBtn.hidden = true;
+            Notify.info("We're sorry, but you've reached the end of search results.");
+        }
        
     }
 }
@@ -72,28 +103,40 @@ function checkQueryResponse(queryResponse){
 function createImageGalleryMarkup(arr){
     console.log("arr=", arr);
 
-    const markup=arr.map(({pageURL, previewURL, tags})=>
+    return arr.map(({largeImageURL, webformatURL, tags, views, likes, comments, downloads})=>
         `<div class="photo-card">
-        <a href="${pageURL}">
-        <img src="${previewURL}" alt="{tags}" loading="lazy" data-source="large-image.jpg"/>
+       
+        <div class="img-thumb">
+        <a href="${largeImageURL}" class="photo-card__link">
+        <img class="photo-card__image"" src="${webformatURL}" alt="{tags}" loading="lazy" data-source="large-image.jpg"/>
         </a>
+        </div>
         <div class="info">
           <p class="info-item">
             <b>Likes</b>
+            <span>${likes}</span>
           </p>
           <p class="info-item">
             <b>Views</b>
+            <span>${views}</span>
           </p>
           <p class="info-item">
             <b>Comments</b>
+            <span>${comments}</span>
           </p>
           <p class="info-item">
             <b>Downloads</b>
+            <span>${downloads}</span>
           </p>
         </div>
       </div>`).join('');
-      
-    console.log(markup);
-    return markup;
     
+}
+
+function onClickLoadMore(){
+    countPage+=1;
+    fetchQuery()
+    .then(checkQueryResponse)
+    .catch(err=>Notify.failure('Ooops! Something went wrong!'));
+
 }
