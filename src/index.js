20ba1d46f6671f4a63c67a5402df5a3bf6330e0c
IOksa <117656767+IOksa@users.noreply.github.com>
2023-04-29 13:56:11 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "35857316-b0404474f9c8f0cfe824c51d8";
 
 const refs={
     form: document.querySelector('#search-form'),
     input: document.querySelector('.search-input'),
+    div: document.querySelector(".gallery"),
 }
+
+let gallery = new SimpleLightbox('.gallery a');
 
 console.log(refs.form);
 console.log(refs.input);
@@ -18,6 +26,7 @@ function onFormSubmit(event){
     const form = event.currentTarget;
     const searchQuery = form.elements.searchQuery.value;
     console.log(searchQuery);
+
     fetchQuery(searchQuery)
     .then(checkQueryResponse)
     .catch(err=>console.error("ERROR!", err));;
@@ -26,20 +35,20 @@ function onFormSubmit(event){
 
 async function fetchQuery(searchQuery){
     console.log("I am in fetchQuery");
-    // try {
-        // const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`);
-        // console.log(response);
-        // const queryResponse = response.json();
-        // console.log('queryResponse=', queryResponse);
-        // return queryResponse;
-    // } catch (error) {
-    //     console.log ("Error!");
-    //     console.error(error);
-    // }
-      
-    // return fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`).then(response=>response.json());
 
-    return axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`);
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+
+    });
+
+    return await axios.get(`${BASE_URL}?${params}`);
+    
+    // return await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`);
+    
 }
 
 function checkQueryResponse(queryResponse){
@@ -51,6 +60,40 @@ function checkQueryResponse(queryResponse){
         Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     }
     else{
-        Notify.info(`${queryResponse.data.hits.length}`);
+        const markup=createImageGalleryMarkup(queryResponse.data.hits);
+        console.log(markup);
+        refs.div.insertAdjacentHTML('beforeend', markup);
+        gallery.refresh();
+    
+       
     }
+}
+
+function createImageGalleryMarkup(arr){
+    console.log("arr=", arr);
+
+    const markup=arr.map(({pageURL, previewURL, tags})=>
+        `<div class="photo-card">
+        <a href="${pageURL}">
+        <img src="${previewURL}" alt="{tags}" loading="lazy" data-source="large-image.jpg"/>
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+          </p>
+        </div>
+      </div>`).join('');
+      
+    console.log(markup);
+    return markup;
+    
 }
